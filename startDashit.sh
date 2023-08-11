00a58c7a52caf56bee24b96c694d176963ff3425
alberto <alberto@corvaglia.eu.org>
2023-08-11 14:28:23 +0200

    added .env file

--- startDashit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JWT_SECRET="7358bd4ec877e1b622397ab4f9706b76680348a2886bfc0a675ed0555085d0ea"
-export ADMIN_USER="admin"
-export ADMIN_PASSWORD="SUCAPERA"
-
-cd /home/alberto/daShit
-screen npm run serverStart
